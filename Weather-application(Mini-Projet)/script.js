e115b9594e27bb4c07ae079136417d402299aa19
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2025-12-10 11:21:48 +0100

    Completion of the weather app projet

--- Weather-application(Mini-Projet)/script.js
+++ Weather-application(Mini-Projet)/script.js
@@ -19,21 +19,110 @@ let time = timeSplit.at(0) + ":" + timeSplit.at(1);
 const weatherTime = document.querySelector(".weather-time");
 const weatherDate = document.querySelector(".weather-date");
 
-const weatherTempurator = document.getElementsByClassName("weather-tempurator");
-const weatherState = document.getElementsByClassName("weather-state");
+const weatherTempurator = document.getElementsByClassName("weather-data-tempurator");
+const weatherState = document.getElementsByClassName("weather-data-state");
 
 const weatherCity = document.querySelector(".weather-city");
 
 const weatherDuration = document.querySelector(".weather-duration");
 
-const weatherFeelsLike = document.querySelector(".weather-feels-like");
+const weatherFeelsLike = document.querySelector(".weather-data-feels-like");
 
 const weatherImage = document.getElementById("weather-image");
+
+const weatherSearchCityBtn = document.getElementById("weather-search-city-btn");
 
 // Building things while the load of page.
 window.onload = buil();
 
+// Initial city will be 'PARIS' as a default city, instead if the user enter a city will rebuild the UI and show the data corresponding to the city.
+fetchWeatherData('paris')
+    .then(response => {
+        let weatherObj = new Weather(
+            response.name,
+            response.main.temp,
+            response.main.humidity,
+            response.weather[0].main,
+            response.main.feels_like,
+        );
+        //clearPreviousUIData();
+        // city.
+        weatherCity.innerText = weatherObj.city;
 
+
+        // Weather Tempurator & State (Cloudy, Suny, Clear ect...).
+        weatherTempurator.item(0).textContent = weatherObj.temperature; 
+        weatherState.item(0).textContent = weatherObj.state;
+
+       weatherTempurator.item(1).textContent = weatherObj.temperature; 
+        weatherState.item(1).textContent =  weatherObj.state;
+
+        weatherFeelsLike.innerText = weatherObj.feelsLike;
+
+        const supElement = document.createElement('sup');
+        weatherFeelsLike.append(supElement)
+
+        let currentWeatherState = checkWeather(weatherObj.state);
+        weatherImage.src = '';
+        weatherImage.src = currentWeatherState;
+    })
+    .catch(error => error);
+
+
+// When the user entring a city and click on the button will rebuild the data corresponding to the city. 
+weatherSearchCityBtn.onclick = () => {
+    const searchCityInput = document.querySelector("input[type='text']");
+    //searchCityInput.value == null ? searchCityInput.value = 'paris': searchCityInput.value;
+    let city = "";
+    if (searchCityInput.value == null) {
+        city = "paris";
+    } else {
+        city = searchCityInput.value;
+    }
+
+fetchWeatherData(city)
+    .then(response => {
+        let weatherObj = new Weather(
+            response.name,
+            response.main.temp,
+            response.main.humidity,
+            response.weather[0].main,
+            response.main.feels_like,
+        );
+        //clearPreviousUIData();
+        // city.
+        weatherCity.innerText = weatherObj.city;
+
+
+        // Weather Tempurator & State (Cloudy, Suny, Clear ect...).
+        weatherTempurator.item(0).textContent = weatherObj.temperature; 
+        weatherState.item(0).textContent = weatherObj.state;
+
+       weatherTempurator.item(1).textContent = weatherObj.temperature; 
+        weatherState.item(1).textContent =  weatherObj.state;
+
+        weatherFeelsLike.innerText = weatherObj.feelsLike;
+
+        const supElement = document.createElement('sup');
+        weatherFeelsLike.append(supElement)
+
+        let currentWeatherState = checkWeather(weatherObj.state);
+        weatherImage.src = '';
+        weatherImage.src = currentWeatherState;
+    })
+    .catch(error => error);
+};
+
+function clearPreviousUIData() {
+    weatherCity.textContent = "";
+    weatherTempurator.item(0).innerHTML ="";
+    weatherTempurator.item(1).innerHTML ="";
+    weatherState.item(0).innerHTML ="";
+    weatherState.item(1).innerHTML ="";
+    weatherFeelsLike.innerHTML="";
+
+
+}
 
 async function fetchWeatherData(city) {
     let endPoint = `${BASEURL}q=${city}&appid=${APIKEY}&units=metric`;
@@ -48,41 +137,6 @@ async function fetchWeatherData(city) {
         console.error('Something went wrong!');
     }
 }
-
-
-fetchWeatherData("london")
-    .then(response => {
-        let weatherObj = new Weather(
-            response.name,
-            response.main.temp,
-            response.main.humidity,
-            response.weather[0].main,
-            response.main.feels_like,
-        );
-
-        // city.
-        weatherCity.innerText = weatherObj.city;
-
-        // Weather Tempurator & State (Cloudy, Suny, Clear ect...).
-        weatherTempurator.item(0).insertAdjacentText("afterbegin", weatherObj.temperature);
-        weatherState.item(0).insertAdjacentText("afterbegin", weatherObj.state);
-
-        weatherTempurator.item(1).insertAdjacentText("afterbegin", weatherObj.temperature);
-        weatherState.item(1).insertAdjacentText("afterbegin", weatherObj.state);
-
-        weatherFeelsLike.insertAdjacentText(
-            "beforeend",
-            weatherObj.feelsLike,
-        );
-
-        const supElement = document.createElement('sup');
-        weatherFeelsLike.appendChild(supElement);
-
-        let currentWeatherState = checkWeather(weatherObj.state);
-        weatherImage.src = '';
-        weatherImage.src = currentWeatherState;
-    })
-    .catch(error => error);
 
 function checkWeather(currentWeatherState) {
     if (currentWeatherState == 'Clear') {
